fa52f2218a38430e25d5b9e2a1f77df73ba45d4c
Garrett Chitwood <gman@acvwa.com>
2025-10-06 15:52:23 -0700

    Completed basic Linq requirements, more to be added in next commit.

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var videoGameNames = new List<string>()
+            {
+                "Stronghold Legends",
+                "Ultimate Spiderman",
+                "Age of Wonders 4",
+                "Ark Survival Evolved",
+                "Dragon Age Inquisition",
+                "Deus Ex Human Revolution",
+                "Dishonored 2",
+                "Dark Souls 3",
+                "The Elder Scrolls V: Skyrim",
+                "Fallout 3",
+                "Tainted Grail The Fall of Avalon"
+            };
+
+            var orderedVideoGames = videoGameNames.OrderBy(videoGameNames => videoGameNames.Length).ToList();
+            orderedVideoGames.ForEach(game => Console.WriteLine(game));
         }
     }
 }
