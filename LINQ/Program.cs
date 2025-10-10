29946d63ec38fbb6658117db3a69a630fbe50851
Garrett Chitwood <gman@acvwa.com>
2025-10-09 18:36:40 -0700

    Added movie list to the solution, to create repetition practice.

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,6 +4,9 @@
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("");
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("Video Game List:\n");
             var videoGameNames = new List<string>()
             {
                 "Stronghold Legends",
@@ -21,6 +24,25 @@
 
             var orderedVideoGames = videoGameNames.OrderBy(videoGameNames => videoGameNames.Length).ToList();
             orderedVideoGames.ForEach(game => Console.WriteLine(game));
+            Console.WriteLine("");
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("Movie List:\n");
+
+            var movieNames = new List<string>()
+            {
+                "Iron Man",
+                "Spiderman",
+                "The Godfather",
+                "The Birds",
+                "Star Wars: Rogue One",
+                "Avatar",
+                "Back to the Future",
+                "Gladiator",
+                "Lelo and Stitch",
+            };
+            
+            var orderedMovieNames = movieNames.OrderBy(movieName => movieName.Length).ToList();
+            orderedMovieNames.ForEach(movieName => Console.WriteLine(movieName));
         }
     }
 }
